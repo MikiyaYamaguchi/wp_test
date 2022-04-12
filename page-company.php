94cd662de1f118daa2dac95b0a4f2de44a5d7ea6
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2022-04-12 15:34:50 +0900

    page-company.phpファイルの削除

--- page-company.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<main>
-	<div class="container">
-		<?php include('bread.php'); ?>
-		<h1><?php the_title(); ?></h1>
-		<?php the_content(); ?>
-	</div>
-</main>
-<?php get_footer(); ?>
